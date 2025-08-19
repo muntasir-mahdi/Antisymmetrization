17f7b94a8b8249c30577023efb39c32da61b0dfe
Muntasir Mahdi <eee.muntasir@gmail.com>
2025-08-19 16:40:25 -0400

    Update Rahe process.py

--- Rahe process.py
+++ Rahe process.py
@@ -1,18 +1,22 @@
-#Processed RAHE data
-
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('ETONew.csv')
+df = pd.read_csv('77K_IP.csv')
 
-#df.head(10)
-df = df.dropna()
-df = df.drop([0]).reset_index()
+#Removing first 2 rows (Unit and Comments)
+df = df.drop([0,1]).reset_index()
 df.drop('index', axis =1, inplace= True)
-#df.head()
 
 df = df.astype(float)
-#df.info()
-rahe = (df.max()-df.min())/2
-R_AHE= rahe.to_frame()
-R_AHE.to_csv('S117_Rahe.csv')
+
+# In[20]:
+#Number of points 21 means loc[0:20]
+for row in df.iterrows():
+    df2 = df.loc[:20].mean().to_frame().T
+
+# In[24]:
+#Number of points to avg = n (len(df)// n)
+df2 = df.groupby(np.arange(len(df)) // 21).mean()
+
+df2.to_csv('New_Processed.csv')
 
